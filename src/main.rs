c2364f656b36825b889e1dead8cd833e35641e7d
Max00009 <mp7636681@gmail.com>
2026-02-18 22:31:13 +0530

    Add separate struct and associated functions for argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
 use std::env;
+use std::fs;
+
+struct Config{
+    query:String,
+    filepath:String,
+}
+impl Config{
+    fn new(args:&[String])->Config{
+        let query=args[1].clone();
+        let filepath=args[2].clone();
+        Config{query,filepath}
+    }
+}
 fn main(){
     let args:Vec<String>=env::args().collect();
-    dbg!(args);
+    let config=Config::new(&args);
+    println!("searching {} in {}",config.query,config.filepath);
+    let contents=fs::read_to_string(config.filepath).expect("should have been opened the file");
 }
