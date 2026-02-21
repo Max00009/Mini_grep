7dbe90fe2436fcdb8c62b399c2e6bf8cca9f0cf6
Max00009 <mp7636681@gmail.com>
2026-02-21 17:53:39 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,14 @@
 use std::env;
-use std::fs;
-
-struct Config{
-    query:String,
-    filepath:String,
-}
-impl Config{
-    fn new(args:&[String])->Config{
-        let query=args[1].clone();
-        let filepath=args[2].clone();
-        Config{query,filepath}
-    }
-}
+use std::process;
+use minigrep::{Config,run};
 fn main(){
-    let args:Vec<String>=env::args().collect();
-    let config=Config::new(&args);
-    println!("searching {} in {}",config.query,config.filepath);
-    let contents=fs::read_to_string(config.filepath).expect("should have been opened the file");
+    let args:Vec<String>=env::args().collect(); //we collect the arguments from command line.
+    let config=Config::new(&args).unwrap_or_else(|err|{     //we send the arguments to new function for parsing.
+        println!("Problem parsing arguments:{}",err);
+        process::exit(1);
+    });
+    if let Err(e)=run(config){  //if run() returns Err intead of Config.
+        println!("Application error:{}",e);
+        process::exit(1);
+    };
 }
